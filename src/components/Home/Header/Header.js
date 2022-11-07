7f40952c151d26bba8bcc712aeb7bffce838ad15
Sayem92 <sayemmiha123@gmail.com>
2022-11-07 23:52:27 +0600

    add header

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,11 +1,63 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import pic from '../../../assets/header.jpg'
 
 const Header = () => {
+
     return (
-        <div>
-            <h1>Header</h1>
+        <div className='mb-20'>
+            <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
+                <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
+                    <svg
+                        className="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block"
+                        viewBox="0 0 100 100"
+                        fill="currentColor"
+                        preserveAspectRatio="none slice"
+                    >
+                        <path d="M50 0H100L50 100H0L50 0Z" />
+                    </svg>
+                    <img
+                        className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
+                        src={pic}
+                        alt=""
+                    />
+                </div>
+                <div className="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
+                    <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
+
+                        <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+                            Everything you
+                            <br className="hidden md:block" />
+                            can imagine{' '}
+                            <span className="inline-block text-orange-400">
+                                is real
+                            </span>
+                        </h2>
+                        <p className="pr-5 mb-5 text-base text-gray-700 md:text-lg">
+                            Photography is the art, application, and practice of creating durable images by recording light, either electronically by means of an image sensor, or chemically by means of a light-sensitive material such as photographic film. It is employed in many fields of science, manufacturing (e.g., photolithography), and business, as well as its more direct uses for art, film and video production, recreational purposes, hobby, and mass communication.
+                        </p>
+                        <div className="flex items-center">
+                            <Link
+                               to="/"
+                                className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-sky-500 hover:bg-sky-600 focus:shadow-outline focus:outline-none"
+                            >
+                                Get started
+                            </Link>
+                            <Link
+                                to="/"
+                                aria-label=""
+                                className="inline-flex items-center font-semibold text-white transition-colors duration-200  h-12 px-6 bg-orange-300 rounded hover:bg-orange-400"
+                            >
+                                Learn more
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
+
+
 
 export default Header;
