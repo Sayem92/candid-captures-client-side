416d7fe26cdf2370b2e3240b8858a526fb74fc77
Sayem92 <sayemmiha123@gmail.com>
2023-01-16 18:34:53 +0600

    update footer

--- src/components/Home/OurTeams/OurTeams.js
+++ src/components/Home/OurTeams/OurTeams.js
@@ -9,58 +9,58 @@ import pic6 from '../../../assets/pic6.jpg'
 
 const OurTeams = () => {
     return (
-        <section class="m-2 lg:mx-16 my-20 bg-white dark:bg-gray-900">
-    <div class="h-[32rem]  dark:bg-gray-800">
-        <div class="container px-6 py-10 mx-auto">
-            <p class="max-w-2xl mx-auto mt-6 text-center text-gray-900 dark:text-gray-300">
+        <section className="m-2 lg:mx-16 my-20 bg-white dark:bg-gray-900">
+    <div className="h-[32rem]  dark:bg-gray-800">
+        <div className="container px-6 py-10 mx-auto">
+            <p className="max-w-2xl mx-auto mt-6 text-center text-gray-900 dark:text-gray-300">
             Our Team
             </p>
 
-            <div class="flex justify-center mx-auto mt-6">
-                <span class="inline-block w-40 h-1 bg-blue-500 rounded-full"></span>
-                <span class="inline-block w-3 h-1 mx-1 bg-blue-500 rounded-full"></span>
-                <span class="inline-block w-1 h-1 bg-blue-500 rounded-full"></span>
+            <div className="flex justify-center mx-auto mt-6">
+                <span className="inline-block w-40 h-1 bg-blue-500 rounded-full"></span>
+                <span className="inline-block w-3 h-1 mx-1 bg-blue-500 rounded-full"></span>
+                <span className="inline-block w-1 h-1 bg-blue-500 rounded-full"></span>
             </div>
             
-            <h1 class="text-3xl mt-6 font-semibold text-center text-orange-500 capitalize lg:text-4xl dark:text-white">Professional Photographers</h1>
+            <h1 className="text-3xl mt-6 font-semibold text-center text-orange-500 capitalize lg:text-4xl dark:text-white">Professional Photographers</h1>
         </div>
     </div>
 
-    <div class="container px-6 py-10 mx-auto -mt-72 sm:-mt-80 md:-mt-96">
-        <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 xl:grid-cols-3">
-            <div class="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700 hover:bg-gray-200">
-                <img class="object-cover w-full rounded-xl aspect-square" src={pic1} alt=""/>
+    <div className="container px-6 py-10 mx-auto -mt-72 sm:-mt-80 md:-mt-96">
+        <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 xl:grid-cols-3">
+            <div className="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700 hover:bg-gray-200">
+                <img className="object-cover w-full rounded-xl aspect-square" src={pic1} alt=""/>
 
-                <h1 class="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">arthur melo</h1>
-                <p class="mt-2 text-blue-600 capitalize dark:text-gray-300">PHOTOGRAPHER</p>
-            </div>
-
-            <div class="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700 hover:bg-gray-200">
-                <img class="object-cover w-full rounded-xl aspect-square" src={pic2} alt=""/>
-
-                <h1 class="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Pamela Anderson</h1>
-                <p class="mt-2 text-blue-600 capitalize dark:text-gray-300">PHOTOGRAPHER</p>
+                <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">arthur melo</h1>
+                <p className="mt-2 text-blue-600 capitalize dark:text-gray-300">PHOTOGRAPHER</p>
             </div>
 
-            <div class="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700 hover:bg-gray-200">
-                <img class="object-cover w-full rounded-xl aspect-square" src={pic3} alt=""/>
-                <h1 class="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Alexandra Dane</h1>
-                <p class="mt-2 text-blue-600 capitalize dark:text-gray-300">PHOTOGRAPHER</p>
+            <div className="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700 hover:bg-gray-200">
+                <img className="object-cover w-full rounded-xl aspect-square" src={pic2} alt=""/>
+
+                <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Pamela Anderson</h1>
+                <p className="mt-2 text-blue-600 capitalize dark:text-gray-300">PHOTOGRAPHER</p>
             </div>
-            <div class="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700 hover:bg-gray-200">
-                <img class="object-cover w-full rounded-xl aspect-square" src={pic4} alt=""/>
-                <h1 class="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Jimmy Thomson</h1>
-                <p class="mt-2 text-blue-600 capitalize dark:text-gray-300">PHOTOGRAPHER</p>
+
+            <div className="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700 hover:bg-gray-200">
+                <img className="object-cover w-full rounded-xl aspect-square" src={pic3} alt=""/>
+                <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Alexandra Dane</h1>
+                <p className="mt-2 text-blue-600 capitalize dark:text-gray-300">PHOTOGRAPHER</p>
             </div>
-            <div class="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700 hover:bg-gray-200">
-                <img class="object-cover w-full rounded-xl aspect-square" src={pic5} alt=""/>
-                <h1 class="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Sabrina Janes</h1>
-                <p class="mt-2 text-blue-600 capitalize dark:text-gray-300">PHOTOGRAPHER</p>
+            <div className="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700 hover:bg-gray-200">
+                <img className="object-cover w-full rounded-xl aspect-square" src={pic4} alt=""/>
+                <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Jimmy Thomson</h1>
+                <p className="mt-2 text-blue-600 capitalize dark:text-gray-300">PHOTOGRAPHER</p>
             </div>
-            <div class="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700 hover:bg-gray-200">
-                <img class="object-cover w-full rounded-xl aspect-square" src={pic6} alt=""/>
-                <h1 class="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">John Doe</h1>
-                <p class="mt-2 text-blue-600 capitalize dark:text-gray-300">PHOTOGRAPHER</p>
+            <div className="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700 hover:bg-gray-200">
+                <img className="object-cover w-full rounded-xl aspect-square" src={pic5} alt=""/>
+                <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Sabrina Janes</h1>
+                <p className="mt-2 text-blue-600 capitalize dark:text-gray-300">PHOTOGRAPHER</p>
+            </div>
+            <div className="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700 hover:bg-gray-200">
+                <img className="object-cover w-full rounded-xl aspect-square" src={pic6} alt=""/>
+                <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">John Doe</h1>
+                <p className="mt-2 text-blue-600 capitalize dark:text-gray-300">PHOTOGRAPHER</p>
             </div>
         </div>
     </div>
